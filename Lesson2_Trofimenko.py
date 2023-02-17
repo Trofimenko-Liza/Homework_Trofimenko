267f0e2e4d4e5019c7d8f153e0e73d3c5ddf7d46
Trofimenko-Liza <elizzar1987@gmail.com>
2023-02-17 20:26:44 +0200

    task 1

--- Lesson2_Trofimenko.py
+++ Lesson2_Trofimenko.py
@@ -1,5 +1,19 @@
 
 
-name = input("Your name: ")
-age = input("Your age: ")
-print(f"Name: {name}  Age: {age}")
+first_name = input("Enter Your first name: ")
+last_name = input("Enter Your last name: ")
+full_name = "\t" + "\n" + first_name + " " + last_name + "\n" + "\t"
+#print(full_name.lower())
+#print(full_name.title())
+#print(first_name*5)
+print(full_name.strip())
+
+
+
+#demo_string = "Trofimenko Liza"
+#print(demo_string.lower())
+#print(demo_string.upper())
+#print(demo_string.capitalize())
+
+
+
