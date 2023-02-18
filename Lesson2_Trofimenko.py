3a314aa18f183eee1cc032ca4b7900b9b754bed8
Trofimenko-Liza <elizzar1987@gmail.com>
2023-02-18 19:20:08 +0200

    add task 3

--- Lesson2_Trofimenko.py
+++ Lesson2_Trofimenko.py
@@ -1,6 +1,6 @@
 import math
 
-
+#task 1
 #first_name = input("Enter Your first name: ")
 #last_name = input("Enter Your last name: ")
 #full_name = "\t" + "\n" + first_name + " " + last_name + "\n" + "\t"
@@ -9,8 +9,7 @@ import math
 #print(first_name*5)
 #print(full_name.strip())
 
-
-
+#task 2
 #radius = int(input("Enter radius: "))
 #diameter = 2*radius
 #C = float(math.pi*diameter)
@@ -18,3 +17,17 @@ import math
 #print(f"Circle Lenght = ", C)
 #print(f"Circle Square = ", S)
 
+
+# test task
+#current_dollar_rate = 37.4532
+#Hruvnya = float(input("Enter quantity of hruvnya: "))
+#Exchange = current_dollar_rate * Hruvnya
+#print(round(Exchange, 2))
+
+
+#task 3
+float_value = 36.56861258
+current_hruvnya_rate = float_value
+dollar = float(input("Enter quantity of dollar: "))
+Exchange = current_hruvnya_rate * dollar
+print(round(Exchange, 2))
