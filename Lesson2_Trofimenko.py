53abc19e847b6f4c9126a41369303ca7c64f4afe
Trofimenko-Liza <elizzar1987@gmail.com>
2023-02-18 18:40:38 +0200

    add task 2

--- Lesson2_Trofimenko.py
+++ Lesson2_Trofimenko.py
@@ -1,19 +1,20 @@
+import math
 
 
-first_name = input("Enter Your first name: ")
-last_name = input("Enter Your last name: ")
-full_name = "\t" + "\n" + first_name + " " + last_name + "\n" + "\t"
+#first_name = input("Enter Your first name: ")
+#last_name = input("Enter Your last name: ")
+#full_name = "\t" + "\n" + first_name + " " + last_name + "\n" + "\t"
 #print(full_name.lower())
 #print(full_name.title())
 #print(first_name*5)
-print(full_name.strip())
+#print(full_name.strip())
 
 
 
-#demo_string = "Trofimenko Liza"
-#print(demo_string.lower())
-#print(demo_string.upper())
-#print(demo_string.capitalize())
-
-
+#radius = int(input("Enter radius: "))
+#diameter = 2*radius
+#C = float(math.pi*diameter)
+#S = float(math.pi*radius**2)
+#print(f"Circle Lenght = ", C)
+#print(f"Circle Square = ", S)
 
