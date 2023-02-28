78f6151f090dd1f13723a6cc871ae26d5aaef3e3
Trofimenko-Liza <elizzar1987@gmail.com>
2023-02-28 19:23:46 +0200

    add task 1, 2, 3

--- Lesson5_Trofimenko.py
+++ Lesson5_Trofimenko.py
@@ -1,3 +1,11 @@
+
+# task 1
+set_1 = {0, 2, 3, 4, 4, 2, 5, 6, 1, 7}
+set_2 = {7, 1, 4, 2, 5, 6, 1, 8,9}
+set_3 = set_1 & set_2
+print(set_1.intersection(set_2) )
+
+
 #task 2
 students_score = {
     "Matvii": 20,
@@ -22,3 +30,7 @@ comand = int(input("Enter user id (1, 2, 3): "))
 print(f"Hello", {user.get(comand, "all")} )
 
 # task 3
+set_4 = {1, 2, 8, 3, 2, 1, 1, 0, 9, 2, 3, 3, 3, 3, 7, 5, 2, }
+print(len(set_4))
+
+# task 4
