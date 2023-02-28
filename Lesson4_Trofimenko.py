f3159a7f4e8bd218a431eaf5fa21cac364a3269e
Trofimenko-Liza <elizzar1987@gmail.com>
2023-02-28 16:36:37 +0200

    Homework

---
+++ Lesson4_Trofimenko.py
@@ -0,0 +1,36 @@
+
+comand = input("enter comand:")
+list_note = []
+
+while comand == "add":
+        add = input("enter new note:")
+        list_note.append(add)
+        print(list_note)
+        comand = input("enter comand:")
+
+        if comand == "earliest":
+                earliest = sorted(list_note)
+                print(earliest)
+                comand = input("enter comand:")
+
+        if comand == "latest":
+                latest = sorted(list_note, reverse = True)
+                print(latest)
+                comand = input("enter comand:")
+
+        if comand == "longest":
+                longest = sorted(list_note, key=len, reverse=True)
+                print(longest)
+                comand = input("enter comand:")
+
+        if comand == "shortest":
+                shortest = sorted(list_note, key=len)
+                print(shortest)
+                comand = input("enter comand:")
+
+                if comand == "exit":
+                        exit = str
+                        break
+
+else:
+        print("Incorrect comand")
