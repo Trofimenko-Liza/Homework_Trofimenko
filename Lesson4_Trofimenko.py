b6ca00202c0350333f8d8bada648ace3a4164c59
Trofimenko-Liza <elizzar1987@gmail.com>
2023-02-28 16:53:03 +0200

    add comment

--- Lesson4_Trofimenko.py
+++ Lesson4_Trofimenko.py
@@ -3,34 +3,41 @@ comand = input("enter comand:")
 list_note = []
 
 while comand == "add":
+        # add new note
         add = input("enter new note:")
         list_note.append(add)
         print(list_note)
         comand = input("enter comand:")
 
         if comand == "earliest":
+        # sorted by earliest->latest and print list
                 earliest = sorted(list_note)
                 print(earliest)
                 comand = input("enter comand:")
 
         if comand == "latest":
+        # sorted by latest->earliest and print list
                 latest = sorted(list_note, reverse = True)
                 print(latest)
                 comand = input("enter comand:")
 
         if comand == "longest":
+        # sorted by lenght, longest->shortest
                 longest = sorted(list_note, key=len, reverse=True)
                 print(longest)
                 comand = input("enter comand:")
 
         if comand == "shortest":
+        # sorted by lenght, shortest->longest
                 shortest = sorted(list_note, key=len)
                 print(shortest)
                 comand = input("enter comand:")
 
         if comand == "exit":
+        # enter exit and break programm
                         exit = str
                         break
 
 else:
+# processing wrong comand
         print("Incorrect comand")
