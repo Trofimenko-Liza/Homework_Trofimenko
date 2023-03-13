36c76b2ff73de9f1d84a2ac5f76c255543cffcec
Trofimenko-Liza <elizzar1987@gmail.com>
2023-03-13 02:22:00 +0200

    add task 2

--- Lesson7_Trofimenko.py
+++ Lesson7_Trofimenko.py
@@ -5,15 +5,28 @@ b = float(input("Enter b: "))
 c = float(input("Enter c: "))
 e = float(input("Enter e: "))
 
-if  ((a*a) + (b*b)) == ((c*c) + (e*e)):
+if  ((a*a) + (b*b)) == ((c*c) + (e*e)):                             # перевірка чи це прямокутник
     rectange_sqrt = a*b
     print("Rectange square is :", +rectange_sqrt)
 
-elif a == b and b == c  and c == e and a ==c and a == e and b == e:
+elif a == b and b == c  and c == e and a ==c and a == e and b == e: # перевірка чи це квадрат
     print("It is square")
 
-else:
+else:                                                               # опрацювання помилки
     print("Incorrect value. It is not Rectange and not Square")
 
-#square = lambda a = input("Enter a: "), b = input("Enter b: "),c = input("Enter c: "), e = input("Enter e: "): True  if a == b == c == e  (a**2)  else False
-#print(square())
+
+
+
+#task 2
+import random
+
+name = ["Anna", "Serhii", "Maxim", "Inna", "Maria", "Ivan", "Matvei", "Maya", "Makar", "Kirill"]
+domain = ["com", "net", "org", "ua", "biz", "info"]
+email_name = (random.choice(name))                                 # генерування випадкового ім'я
+email_domain = (random.choice(domain))                             # генерування випадкового домейну
+email_data = (str(random.randint(99,1000)))                        # генерування випадкового числа
+email_letter = [random.choice('abcdefghijklm') for i in range(7)]  # генерування набору випадкових літер
+email_letter2 = ''.join(email_letter)                              # приведення випадковіх літер до читабельного вигляду
+full_email = str(email_name +"." + email_data + "@" + email_letter2 +"." +  email_domain) # компанування всіх значень
+print(full_email)
