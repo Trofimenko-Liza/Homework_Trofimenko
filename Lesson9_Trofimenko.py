cf65a406b8080386c20cc47d535fee070d68a06b
Trofimenko-Liza <elizzar1987@gmail.com>
2023-03-22 19:37:56 +0200

    add task 1

---
+++ Lesson9_Trofimenko.py
@@ -0,0 +1,10 @@
+import re
+
+
+result = re.search('^\w.*\w$', 'This is 4 a test homework 25647 5_')
+print(result)
+
+
+#pattern = re.compile('\d')
+#result2 = pattern.split(['example (.com)', 'github (.com)', 'stackoverflow (.com)'])
+
