2e3106343f51b3ae59d2910da6c9c5ac9faa599c
Trofimenko-Liza <elizzar1987@gmail.com>
2023-04-04 14:55:55 +0300

    add task 4 and edit task 3

--- lesson_12/Homework8_Trofimenko.py
+++ lesson_12/Homework8_Trofimenko.py
@@ -1,5 +1,7 @@
 import datetime
 
+
+
 # task 2
 def sum(a, b):
     try:
@@ -10,9 +12,19 @@ sum(1, 2)
 
 
 # task 3
-time1 = datetime.datetime(year=2022, month=2, day=24)
-time2 = datetime.timedelta(days=-100)
-time3 = time1 - time2
-print(time3)
+def changed_time():
+    start = datetime.datetime(year=2022, month=2, day=24)
+    difference = datetime.timedelta(days=-404)
+    return(start - difference)
+changed_time()
+print(changed_time())
+
+# task 4
+def years_old():
+    birth_time = datetime.datetime(year=2011, month=1, day=30)
+    today = datetime.datetime.now()
+    return(today - birth_time)
+years_old()
+print(years_old())
 
 
