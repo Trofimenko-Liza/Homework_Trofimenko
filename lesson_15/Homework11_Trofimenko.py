840582cbc89b8e44a96bc8d948021b34c354815e
Trofimenko-Liza <elizzar1987@gmail.com>
2023-04-11 22:43:12 +0300

    add task 2

--- lesson_15/Homework11_Trofimenko.py
+++ lesson_15/Homework11_Trofimenko.py
@@ -1,5 +1,4 @@
-
-
+# task 1
 class Dog:
     def __init__(self, breed, name, age, colour, gender):
         self.breed = breed
@@ -21,4 +20,37 @@ vote = Dog.say_woof()
 print(dog2.breed, dog2.name, dog2.gender)
 print(dog1.name, dog1.age, dog1.colour)
 print(dog3.breed, dog3.name, dog3.age, dog3.colour, dog3.gender)
+
+
+
+# task 2
+
+class Scrumcompany:
+    def __init__(self,
+                 first_name:str=None,
+                 last_name:str=None,
+                 city:str=None,
+                 title:str=None,
+                 work_experience:int=None
+                 ):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.city = city
+        self.title = title
+        self.work_experience = work_experience
+
+    @classmethod
+    def new_employee(cls, first_name, last_name, title):
+        return(first_name, last_name, title)
+
+employee1 = Scrumcompany('Eva', 'Martynenko', 'Kharkiv', 'Account_Manager', 1)
+employee2 = Scrumcompany('Alex', 'Stepanuk', 'Poltava', 'Java_developer', 4)
+employee3 = Scrumcompany('Bogdana', 'Urchenko', 'Kyiv', 'HR', 3)
+
+new_employee = Scrumcompany('Natalia', 'Butsko', 'Designer')
+
+print(employee1.first_name, employee1.last_name, employee1.title)
+print(new_employee.last_name)
+
+
 
