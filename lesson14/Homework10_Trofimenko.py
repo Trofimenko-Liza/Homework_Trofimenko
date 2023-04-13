e48a916ab0a39b5d86f4797258349ee6decd5d86
Trofimenko-Liza <elizzar1987@gmail.com>
2023-04-13 14:21:11 +0300

    add task 1

--- lesson14/Homework10_Trofimenko.py
+++ lesson14/Homework10_Trofimenko.py
@@ -1,5 +1,34 @@
-import random
 
+# task 1
+def summa(func):
+    def inner():
+        print('This is summa')
+        func()
+        print(func())
+
+    return inner
+
+
+def multy(func):
+    def inner():
+        print('This is multy')
+        func()
+        print(func())
+
+    return inner
+
+
+@summa
+def calculate_sum(a=3, b=6):
+    return a + b
+
+calculate_sum()
+
+@multy
+def calculate_multy(a=5, b=7):
+    return a * b
+
+calculate_multy()
 
 
 
@@ -7,10 +36,15 @@ import random
 
 
 # task 2
+
+import random
+
 item_count = {}
 new_gen = [random.randint(1,10) for item in range(100)]
 print(new_gen)
 for i in new_gen:
     item_count[i] = new_gen.count(i)
     print(item_count)
+
+
 
