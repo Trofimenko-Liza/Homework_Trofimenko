6d519a7bbffc14f8f95db5e050e0b833d59ef938
Trofimenko-Liza <elizzar1987@gmail.com>
2023-04-29 14:54:17 +0300

    add Homework 13

---
+++ lesson_17/Homework13_Trofimenko.py
@@ -0,0 +1,99 @@
+from abc import ABC, abstractmethod
+
+class Museum(ABC):
+    def __init__(self):
+        self.exhibit = None
+        self.picture = None
+        self.household_item = None
+
+    @abstractmethod
+    def add_new_exhibit(self):
+        pass
+
+class Ukranian_section(Museum):
+    def __init__(self):
+        super().__init__()
+        self.exhibit = 'Scythian gold'
+        self.picture = 'Black Square Malevich'
+        self.household_item = 'Clothes of Kievska Rus'
+
+    def add_new_exhibit(self):
+        print('Kobzar Shevchenko')
+
+class Italian_section(Museum):
+    def __init__(self):
+        super().__init__()
+        self.exhibit = 'David Michelangelo'
+        self.picture = 'The Last Supper by Leonardo da Vinci'
+        self.household_item = 'Ancient Rome clothing'
+
+    def add_new_exhibit(self):
+        print('Sistine Madonna')
+
+class France_section(Museum):
+    def __init__(self):
+        super().__init__()
+        self.exhibit = 'Venus Milos'
+        self.picture = 'Mona Lisa'
+        self.household_item = 'Clothes of ancient Galia'
+        self.__new_exhibit = 0
+
+    def add_new_exhibit(self):
+        self.__prepare_place()
+        self.__get_permissions_to_delivery()
+        self.__get_logistic()
+        print('Victoire de Samothrace')
+        self.__new_exhibit += 1
+
+    def __prepare_place(self):
+        print('Prepare a place for a new exhibit')
+
+    def __get_permissions_to_delivery(self):
+        print('Obtaining permits for moving the exhibit')
+
+    def __get_logistic(self):
+        print('Prepare transport for moving the exhibit')
+
+
+    @property
+    def add_exhibit(self):
+        return self.__new_exhibit
+
+# не могу добавить сеттер
+'''
+    #@exhibit.setter
+    #def exhibit(self, new_exponat):
+    #    self.__exhibit = new_exponat
+
+
+#new_exponat = Museum('USA', 'picture2', 'clothes1')
+#new_exponat.exhibit = 'Bible'
+#print(new_exponat.exhibit)
+'''
+
+
+ukrainian_museum = Ukranian_section()
+
+print(ukrainian_museum.household_item)
+print(ukrainian_museum.picture)
+ukrainian_museum.exhibit
+
+italian_museum = Italian_section()
+
+italian_museum.picture
+print(italian_museum.picture)
+
+france_museum = France_section()
+
+print(france_museum.picture)
+france_museum.add_new_exhibit()
+
+ukrainian_museum.add_new_exhibit()
+italian_museum.add_new_exhibit()
+
+
+
+
+
+
+
