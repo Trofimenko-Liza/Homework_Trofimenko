698924261e5c5dada1d6c94e2eb3d258e33935f4
Trofimenko-Liza <elizzar1987@gmail.com>
2023-05-04 18:59:28 +0300

    add test

---
+++ lesson_20/Homework16_Trofimenko/test_adapter_example.py
@@ -0,0 +1,10 @@
+from lesson_20.Homework16_Trofimenko.adapter_example import new_list
+
+class TestAddNewList:
+    def test_add_newlist(self):
+        my_list = ['Alex', 'Shevchenko', 25, 'Java']
+        assert my_list == new_list
+
+
+
+
